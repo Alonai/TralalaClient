3d3bc5bf72b06d952144275d2846483a1ab90ed8
Seven Flying <diez.f.irene@gmail.com>
2014-01-16 15:26:30 +0100

    args para las facades + build

--- src/launcher/ApplicationController.java
+++ src/launcher/ApplicationController.java
@@ -5,6 +5,11 @@ import gui.SongPlayerGUI;
 public class ApplicationController {
 
 	public static void main(String[] argv){
+		// Datos para hacer rebind en facades
+		String namePayFac= "//" + argv[0] + ":" + argv[1] + "/" + argv[2];
+		String nameSongFac = "//" + argv[3] + ":" + argv[4] + "/" + argv[5];
+		String nameManageFac = "//" + argv[6] + ":" + argv[7] + "/" + argv[8];
+		//TODO que el song player gui tenga estos 3 strings
 		new SongPlayerGUI("//" + argv[0] + ":" + argv[1] + "/" + argv[2]);
 		
 	}
